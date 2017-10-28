f2b310f88099f151a782b49bfc8b924503b19da8
lavee singh <laveechaudharytheone@gmail.com>
2017-10-28 17:00:03 +0530

    fix: modified settings for database

--- dkmentions/settings.py
+++ dkmentions/settings.py
@@ -75,9 +75,9 @@ WSGI_APPLICATION = 'dkmentions.wsgi.application'
 DATABASES = {
     'default': DATABASE_DEFAULT
 }
-import dj_database_url
-db_from_env = dj_database_url.config()
-DATABASES['default'].update(db_from_env)
+# import dj_database_url
+# db_from_env = dj_database_url.config()
+# DATABASES['default'].update(db_from_env)
 
 
 # Password validation
