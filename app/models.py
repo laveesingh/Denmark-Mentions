bee1103145ab3a3195ab11526ca54ef50d0ae602
laveesingh <laveechaudharytheone@gmail.com>
2017-11-03 23:34:48 +0530

    fix: deleted model representations to avoid unicode encoding error

--- app/models.py
+++ app/models.py
@@ -11,11 +11,17 @@ class Ytcomment(models.Model):
     video = models.CharField(max_length=100, default="", null=True)
     # channel = models.CharField(max_length=100, default="", null=True)
 
-    def __repr__(self):
-        return self.username.encode('utf-8') + ": " + self.message.encode('utf-8')
+    # def __repr__(self):
+        # try:
+            # return self.username.encode('utf-8') + ": " + self.message.encode('utf-8')
+        # except:
+            # return "YTCOMMENT"
 
-    def __unicode__(self):
-        return self.username.encode('utf-8') + ": " + self.message.encode('utf-8')
+    # def __unicode__(self):
+        # try:
+            # return self.username.encode('utf-8') + ": " + self.message.encode('utf-8')
+        # except:
+            # return "YTCOMMENT"
 
 
 class Fbpost(models.Model):
@@ -24,11 +30,14 @@ class Fbpost(models.Model):
     pagename = models.CharField(max_length=100, default="", null=True)
     timestamp = models.DateTimeField(unique=False)
     
-    def __repr__(self):
-        return self.pagename.encode('utf-8') + ": " + self.timestamp.encode('utf-8')
+    # def __repr__(self):
+        # try:
+            # return self.pagename.encode('utf-8') + ": " + self.timestamp.encode('utf-8')
+        # except:
+            # return "FBPOST"
 
-    def __unicode__(self):
-        return self.pagename.encode('utf-8') + ": " + self.timestamp.encode('utf-8')
+    # def __unicode__(self):
+        # return self.pagename.encode('utf-8') + ": " + self.timestamp.encode('utf-8')
 
 
 class Fbcomment(models.Model):
@@ -38,12 +47,15 @@ class Fbcomment(models.Model):
     timestamp = models.DateTimeField(unique=False)
     # post = models.ForeignKey(Fbpost, on_delete=models.CASCADE)
 
-    def __repr__(self):
-        return self.username.encode('utf-8') + ": " + self.message.encode('utf-8')
+    # def __repr__(self):
+        # try:
+            # return self.username.encode('utf-8') + ": " + self.message.encode('utf-8')
+        # except:
+            # return "FBCOMMENT"
 
 
-    def __unicode__(self):
-        return self.username.encode('utf-8') + ": " + self.message.encode('utf-8')
+    # def __unicode__(self):
+        # return self.username.encode('utf-8') + ": " + self.message.encode('utf-8')
 
 
 class ObjectHash(models.Model):
