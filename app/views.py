fcbe1f5195b13545b69b8b8fb2198bf556c250d6
laveesingh <laveechaudharytheone@gmail.com>
2017-12-02 15:58:11 +0530

    feat: enable updating users list in archive

--- app/views.py
+++ app/views.py
@@ -12,6 +12,7 @@ from django.http import HttpResponse, JsonResponse
 from scripts.facebook_script import scrape_facebook
 from scripts.youtube_script import scrape_youtube
 from scripts.twitter_script import scrape_twitter
+from app.update_json import handle_user_update
 
 
 def update(request):
@@ -39,3 +40,9 @@ def update(request):
     ).start()
     return JsonResponse({'msg': 'update in progress'})
 
+
+def user_list_update(request):
+    data = request.GET
+    user_list = data.get('user_list').split()
+    handle_user_update(user_list)
+    return JsonResponse({'msg': 'successfully updated the list'})
--- dkmentions/urls.py
+++ dkmentions/urls.py
@@ -8,7 +8,8 @@ from app import views as app_views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.main),
-    url(r'^update', app_views.update)
+    url(r'^update', app_views.update),
+    url(r'^user_list_update', app_views.user_list_update)
 ]
 
 urlpatterns += staticfiles_urlpatterns()
